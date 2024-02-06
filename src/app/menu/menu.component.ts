379f6c62ef0eff1cc07980a776f4339975c67650
Gabriel Lopes <glopesagiova@gmail.com>
2024-02-06 20:14:44 -0300

    first commit

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css'],
+})
+export class MenuComponent {
+    
+}
